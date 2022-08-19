1afa5909a9e441d269b410fa907fd6a814bcb4f1
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-08-19 17:31:46 +0300

    Add: Sketches for main menu layout

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,11 @@
+const Menu = () => {
+  return (
+    <div className="Menupage">
+      <div className="Menubtn">Start Game</div>
+      <div className="Menubtn">View Highscores</div>
+      <div className="Menubtn">Options</div>
+    </div>
+  );
+};
+
+export default Menu;
