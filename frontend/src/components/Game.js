823429e84fa01cb1eddcf4697ff59c2ea40b50d6
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-08-21 14:31:47 +0300

    Add: canvas now draws a moving rectangle

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,9 +1,30 @@
+import { useEffect, useRef } from "react";
+
+let x = 0;
 const Game = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const render = () => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = "green";
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.fillRect(x, 0, 30, 30);
+      x++;
+      ctx.stroke();
+      requestAnimationFrame(render);
+    };
+
+    render();
+  }, []);
+
   return (
     <div className="gameScreen">
       <div className="infoBoard">info here</div>
       <div className="canvasCrate">
-        <canvas id="canvas"></canvas>
+        <canvas id="canvas" ref={canvasRef}></canvas>
       </div>
 
       <div className="gameControls">controls here</div>
