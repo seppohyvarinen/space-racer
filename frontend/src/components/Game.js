68c44685608725e3fb6f5944f1e40e155906349e
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-08-21 13:50:40 +0300

    Add: canvas and layout for game screen

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,5 +1,14 @@
 const Game = () => {
-  return <div className="gameScreen">Welcome</div>;
+  return (
+    <div className="gameScreen">
+      <div className="infoBoard">info here</div>
+      <div className="canvasCrate">
+        <canvas id="canvas"></canvas>
+      </div>
+
+      <div className="gameControls">controls here</div>
+    </div>
+  );
 };
 
 export default Game;
