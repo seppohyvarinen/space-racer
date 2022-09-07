c707f3d155cd14e3adb2008477d72c342c13540e
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-09-07 16:36:00 +0300

    Add: functionality to game screen, player now can move space ship to left or right

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,20 +1,64 @@
 import { useEffect, useRef } from "react";
 
-let x = 0;
+let x = 230;
+let leftPressed = false;
+let rightPressed = false;
 const Game = () => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const render = () => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      ctx.fillStyle = "green";
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
-      ctx.fillRect(x, 0, 30, 30);
-      x++;
-      ctx.stroke();
-      requestAnimationFrame(render);
+      const handleKeyDown = (event) => {
+        let key;
+        if (event !== undefined) {
+          key = event.keyCode;
+        }
+
+        if (key == 37) {
+          event.preventDefault();
+          leftPressed = true;
+        }
+        if (key == 39) {
+          event.preventDefault();
+          rightPressed = true;
+        }
+      };
+
+      const handleKeyUp = (event) => {
+        let key;
+        if (event !== undefined) {
+          key = event.keyCode;
+        }
+
+        if (key == 37) {
+          leftPressed = false;
+        }
+        if (key == 39) {
+          rightPressed = false;
+        }
+      };
+      document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("keyup", handleKeyUp);
+
+      let ship = new Image();
+      ship.src = require("../assets/ship.png");
+      ship.onload = () => {
+        const canvas = canvasRef.current;
+        canvas.width = 600;
+        canvas.height = 800;
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.drawImage(ship, x, 650, 140, 140);
+        if (leftPressed) {
+          x--;
+        }
+        if (rightPressed) {
+          x++;
+        }
+
+        requestAnimationFrame(render);
+      };
     };
 
     render();
